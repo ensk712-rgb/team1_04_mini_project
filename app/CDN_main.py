b7c629a16aa4fa82c576e69f488797cffed91d00
Choiduna <ensk712@gmail.com>
2026-07-22 15:26:56 +0900

    main

--- app/CDN_main.py
+++ app/CDN_main.py
@@ -1,10 +1,13 @@
 from fastapi import FastAPI
-from app.routers.chat_router import chat_router
-from app.routers.product_router import product_router
+# from app.routers.chat_router import chat_router
+# from app.routers.product_router import product_router
+from app.routers.inquiry_router import *
+from app.routers.member_router import *
+from app.routers.menu_router import *
 import app.core.chat_config  
 
 app = FastAPI(title="Main App")
 
-app.include_router(chat_router)
-app.include_router(product_router)
-
+app.include_router(inquiry_router)
+app.include_router(member_router)
+app.include_router(menu_router)
